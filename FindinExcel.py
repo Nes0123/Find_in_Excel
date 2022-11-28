b77461459c3e44b201ce12cd520847bfe27252e8
Moonseok Kim <neskimms@naver.com>
2022-11-28 21:59:11 +0900

    first commit

---
+++ FindinExcel.py
@@ -0,0 +1,30 @@
+
+import os
+from tkinter import *
+import tkinter.ttk as ttk
+from tkinter import filedialog
+import tkinter.messagebox as msgbox
+
+import os
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
